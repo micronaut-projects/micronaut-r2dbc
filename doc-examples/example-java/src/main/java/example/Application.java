48b801830afb3395a2d045ff99eb36e564429773
graemerocher <graeme.rocher@gmail.com>
2020-10-29 14:16:48 +0100

    Start prep for GraalVM support

--- doc-examples/example-java/src/main/java/example/Application.java
+++ doc-examples/example-java/src/main/java/example/Application.java
@@ -1,30 +1,8 @@
 package example;
 
-import io.micronaut.context.event.StartupEvent;
-import io.micronaut.r2dbc.rxjava2.RxConnectionFactory;
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.runtime.event.annotation.EventListener;
 
-import javax.inject.Singleton;
-
-@Singleton
 public class Application {
-    private final RxConnectionFactory connectionFactory;
-
-    public Application(RxConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-    }
-
-    @EventListener
-    void onStartup(StartupEvent event) {
-        connectionFactory.withTransaction((connection) -> connection.createBatch()
-            .add("CREATE TABLE BOOKS(TITLE VARCHAR(255), PAGES INT)")
-            .add("INSERT INTO BOOKS(TITLE, PAGES) VALUES ('The Stand', 1000)")
-            .add("INSERT INTO BOOKS(TITLE, PAGES) VALUES ('The Shining', 400)")
-            .execute()
-        ).blockingSubscribe();
-    }
-
     public static void main(String...args) {
         Micronaut.run(args);
     }
--- doc-examples/example-java/src/test/java/example/BookControllerTest.java
+++ doc-examples/example-java/src/test/java/example/BookControllerTest.java
@@ -1,22 +1,36 @@
 package example;
 
-import io.micronaut.context.annotation.Property;
+import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.r2dbc.rxjava2.RxConnectionFactory;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.test.support.TestPropertyProvider;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
+import javax.inject.Inject;
 import java.util.List;
+import java.util.Map;
 
 @MicronautTest
-@Property(name = "r2dbc.datasources.default.url", value = "r2dbc:tc:postgresql:///databasename?TC_IMAGE_TAG=9.6.8")
-public class BookControllerTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BookControllerTest implements TestPropertyProvider {
 
-    private final BookClient bookClient;
+    @Inject BookClient bookClient;
 
-    public BookControllerTest(BookClient bookClient) {
-        this.bookClient = bookClient;
+    @BeforeAll
+    static void setupData(RxConnectionFactory connectionFactory) {
+        connectionFactory.withTransaction((connection) -> connection.createBatch()
+                .add("CREATE TABLE BOOKS(TITLE VARCHAR(255), PAGES INT)")
+                .add("INSERT INTO BOOKS(TITLE, PAGES) VALUES ('The Stand', 1000)")
+                .add("INSERT INTO BOOKS(TITLE, PAGES) VALUES ('The Shining', 400)")
+                .execute()
+        ).blockingSubscribe();
     }
 
     @Test
@@ -25,6 +39,20 @@ public class BookControllerTest {
         Assertions.assertEquals(
                 2,
                 list.size()
+        );
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        PostgreSQLContainer<?> container = new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag(PostgreSQLContainer.DEFAULT_TAG));
+        container.start();
+        return CollectionUtils.mapOf(
+                "r2dbc.datasources.default.host", container.getHost(),
+                "r2dbc.datasources.default.port", container.getFirstMappedPort(),
+                "r2dbc.datasources.default.driver", "postgresql",
+                "r2dbc.datasources.default.username", container.getUsername(),
+                "r2dbc.datasources.default.password", container.getPassword(),
+                "r2dbc.datasources.default.database", container.getDatabaseName()
         );
     }
 
