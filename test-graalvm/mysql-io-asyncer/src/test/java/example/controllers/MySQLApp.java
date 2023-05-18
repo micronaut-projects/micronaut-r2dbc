a668973ba43b9266d4c3b2c4f5d5e814ee07b994
radovanradic <radicr@gmail.com>
2023-05-18 14:04:09 +0200

    Add new mysql driver to the catalog, not to be used in data project

---
+++ test-graalvm/mysql-io-asyncer/src/test/java/example/controllers/MySQLApp.java
@@ -0,0 +1,9 @@
+package example.controllers;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("example.controllers")
+public class MySQLApp {
+}
