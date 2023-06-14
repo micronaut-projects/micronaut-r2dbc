f5f6319c5c6c160cd418202cd4f17be84cdf5e93
radovanradic <radicr@gmail.com>
2023-06-14 09:09:45 +0200

    Fix compile errors

--- test-graalvm/common/src/main/java/testgraalvm/repositories/PetRepository.java
+++ test-graalvm/common/src/main/java/testgraalvm/repositories/PetRepository.java
@@ -6,10 +6,10 @@ import reactor.core.publisher.Mono;
 import testgraalvm.controllers.dto.PetDto;
 import testgraalvm.domain.Pet;
 import io.micronaut.transaction.TransactionDefinition;
-import io.micronaut.transaction.annotation.TransactionalAdvice;
+import io.micronaut.transaction.annotation.Transactional;
 import io.r2dbc.spi.Connection;
 
-@TransactionalAdvice(isolation = TransactionDefinition.Isolation.SERIALIZABLE)
+@Transactional(isolation = TransactionDefinition.Isolation.SERIALIZABLE)
 public interface PetRepository extends ReactorCrudRepository<Pet, Long> {
     Flux<PetDto> list();
 
--- test-graalvm/oracle/src/main/java/example/repositories/OracleOwnerRepository.java
+++ test-graalvm/oracle/src/main/java/example/repositories/OracleOwnerRepository.java
@@ -3,10 +3,10 @@ package example.repositories;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
 import io.micronaut.transaction.TransactionDefinition;
-import io.micronaut.transaction.annotation.TransactionalAdvice;
+import io.micronaut.transaction.annotation.Transactional;
 import testgraalvm.repositories.OwnerRepository;
 
 @R2dbcRepository(dialect = Dialect.ORACLE)
-@TransactionalAdvice(isolation = TransactionDefinition.Isolation.DEFAULT)
+@Transactional(isolation = TransactionDefinition.Isolation.DEFAULT)
 public interface OracleOwnerRepository extends OwnerRepository {
 }
