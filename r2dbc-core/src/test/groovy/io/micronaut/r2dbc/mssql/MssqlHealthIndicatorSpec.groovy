9a9250fdc3b86409333a156f75a5dc14c4d1e0d4
radovanradic <radicr@gmail.com>
2025-03-14 11:15:47 +0100

    Try latest MSSQL container

--- r2dbc-core/src/test/groovy/io/micronaut/r2dbc/mssql/MssqlHealthIndicatorSpec.groovy
+++ r2dbc-core/src/test/groovy/io/micronaut/r2dbc/mssql/MssqlHealthIndicatorSpec.groovy
@@ -13,7 +13,7 @@ import jakarta.inject.Inject
 import spock.lang.Specification
 
 @MicronautTest
-@Property(name = "r2dbc.datasources.default.url", value = "r2dbc:tc:sqlserver:///?TC_IMAGE_TAG=2017-CU12")
+@Property(name = "r2dbc.datasources.default.url", value = "r2dbc:tc:sqlserver:///?TC_IMAGE_TAG=2022-latest")
 @Property(name = "r2dbc.datasources.default.options.applicationName", value = "test")
 class MssqlHealthIndicatorSpec extends Specification {
     @Inject BasicR2dbcProperties props
