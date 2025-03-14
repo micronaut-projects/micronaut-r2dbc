a277c8990020fab71bc4156a689f4f875c193bcb
Radovan Radic <radicr@gmail.com>
2025-03-14 10:50:35 +0100

    Remove mysql test for dev-miku library no longer being maintained and was replaced by asyncer-io (#746)

--- test-graalvm/mysql-io-asyncer/src/test/java/example/controllers/MySQLApp.java
+++
@@ -1,9 +0,0 @@
-package example.controllers;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectPackages("example.controllers")
-public class MySQLApp {
-}
