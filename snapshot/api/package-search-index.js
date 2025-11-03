8619d1e1333458690f05d3704efb228841b7067b
sdelamo <sdelamo@users.noreply.github.com>
2025-11-03 13:21:56 +0000

    Deploying to gh-pages - 13:21:56

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.r2dbc"},{"l":"io.micronaut.r2dbc.config"},{"l":"io.micronaut.r2dbc.health"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.r2dbc"},{"l":"io.micronaut.r2dbc.config"},{"l":"io.micronaut.r2dbc.health"},{"l":"testgraalvm"},{"l":"testgraalvm.controllers.dto"},{"l":"testgraalvm.domain"},{"l":"testgraalvm.repositories"}];updateSearchResults();
